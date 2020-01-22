68767c9c5d80d756368b6227ae9e1b6ba784a88c
Neciu Dan <dan@adoreme.com>
2020-01-22 21:39:02 +0200

    Fixed bugs. Functionality is complete. Needs refactoring, tests and style encapsoulation

--- src/components/autocomplete.jsx
+++ src/components/autocomplete.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect, useRef } from 'react';
 import './autocomplete.scss';
 
 const Label = (props) => {
@@ -22,14 +22,24 @@ const ItemInfo = props => {
     )
 }
 const Dropdown = (props) => {
-    const selectItem = (item) => {
-        console.log(item) 
+    let refs = useRef(Array.from({ length: props.data.length }).map(() => React.createRef()));
+    
+    const selectItem = (item, i ) => {
+        props.emit(item.name)
+        console.log(refs);
     }
+    useEffect(() => {
+        if (props.active > 2) {
+            refs.current[props.active - 1].current.scrollIntoView();
+        } else {
+            refs.current[0].current.scrollIntoView();
+        }
+    }, [props.active]);
     return (
         <Fragment>
             <ul className="autocomplete__results">
                 {props.data && props.data.map((item, i) => (
-                    <li key={i} className="autocomplete__results-item" onClick={() => selectItem(item)}>
+                    <li key={i + 1} ref={refs.current[i]} className={'autocomplete__results-item ' + (props.active === i + 1 ? 'autocomplete__results-item--active' : '' )} onClick={() => selectItem(item, i)}>
                         <Avatar avatar={item.avatar} initials={item.initials}  /> 
                         <ItemInfo name={item.name} email={item.email} />
                     </li>
@@ -42,7 +52,42 @@ const Dropdown = (props) => {
 const Autocomplete = (props) => {
     const [inputText, setInputText] = useState('');
     const [active, setActive] = useState(false);
-    
+    const [filteredData, setFilterData] = useState([])
+    const [activeItem, setActiveItem] = useState(0)
+
+    const filterData = (value) => {
+        const matchingItems = props.data.filter((item) => {
+            if(item.name.replace(/\s/g,'').toLowerCase().indexOf(value.replace(/\s/g,'').toLowerCase()) > -1) {
+                return true
+            }
+            return false
+        });
+
+        setFilterData(matchingItems);
+        setInputText(value);
+    }
+    const handleKeyDown = (event) => {
+        if(event.key === 'Enter') {
+            setActiveItem(0)
+            setInputText(filteredData[activeItem].name)
+            setFilterData(props.data)
+            setActive(false)
+        }
+        if(event.key === 'ArrowDown') {
+            if(activeItem < filteredData.length) {
+                setActiveItem(activeItem + 1)
+            }
+        }
+        if(event.key === 'ArrowUp') {
+            if(activeItem > 0) {
+                setActiveItem(activeItem - 1)
+            }
+        }
+    }
+    useEffect(() => {
+        setFilterData(props.data);
+    }, [props.data]);
+
     return (
         <Fragment>
             <div className={'autocomplete ' + ( active ? 'autocomplete--active' : 'autocomplete--inactive') }>
@@ -51,13 +96,16 @@ const Autocomplete = (props) => {
                     type="text"  
                     className="autocomplete__input" 
                     value={inputText}  
-                    onChange={(event) => setInputText(event.target.value)} 
+                    onChange={(event) => filterData(event.target.value)}
+                    onKeyDown={handleKeyDown}
                     onFocus={() => setActive(true)} 
                     onBlur={() => setActive(false)} 
                     placeholder={props.placeholder}
                 />
                 <div className="autocomplete__caret"></div>
-                <Dropdown data={props.data} />
+                {filteredData.length > 0 && 
+                <Dropdown active={activeItem} data={filteredData} emit={setInputText} />
+                }
             </div>
             
         </Fragment>
