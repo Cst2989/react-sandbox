52ebf46cdb20fa7d732a9fc4b9852ba7ff874bf4
Neciu Dan <dan@adoreme.com>
2020-01-23 14:36:30 +0200

    Made text work

--- src/components/autocomplete.test.js
+++ src/components/autocomplete.test.js
@@ -1,32 +1,72 @@
-import React, { useState } from "react";
+import React from 'react';
 import ReactDOM from "react-dom";
-import { act } from "react-dom/test-utils";
-let container;
-beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-});
-afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-});
-function Button(props) {
-    const [text, setText] = useState("");
-    function handleClick() {
-        setText("PROCEED TO CHECKOUT");
+import { shallow } from 'enzyme';
+import Autocomplete from './Autocomplete';
+import { create, act } from "react-test-renderer";
+const MockedData = [
+    {
+        name: 'Dan Neciu',
+        initials: 'DN',
+        avatar: null,
+        email: 'dan.neciu@fakee.com'
+    },
+    {
+        name: 'Joe Johnson',
+        initials: 'JJ',
+        avatar: null,
+        email: 'joe@fakee.com'
+    },
+    {
+        name: 'Ion Ionescu',
+        initials: 'IJ',
+        avatar: null,
+        email: 'fake@fakee.com'
+    },
+    {
+        name: 'Donald Trump',
+        initials: 'DT',
+        avatar: null,
+        email: 'donald@fakee.com'
+    },
+    {
+        name: 'JR Smith',
+        initials: 'JR',
+        avatar: null,
+        email: 'jerry@fakee.com'
     }
-    return <button onClick={handleClick}>{text || props.text}</button>;
+];
+const createNodeMock = element => {
+    if (element.type === 'li') {
+        // This is your fake DOM node for <p>.
+        // Feel free to add any stub methods, e.g. focus() or any
+        // other methods necessary to prevent crashes in your components.
+        return {
+            scrollIntoView : function (){
+                
+            }
+        };
+    }
+    // You can return any object from this method for any type of DOM component.
+    // React will use it as a ref instead of a DOM node when snapshot testing.
+    return null;
 }
-describe("Button component", () => {
-    test("it shows the expected text when clicked", () => {
-        act(() => {
-            ReactDOM.render(<Button text="SUBSCRIBE TO BASIC" />, container);
-        });
-        const button = container.getElementsByTagName("button")[0];
-        expect(button.textContent).toBe("SUBSCRIBE TO BASIC");
-        act(() => {
-            button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-        });
-        expect(button.textContent).toBe("PROCEED TO CHECKOUT");
-    });
+it('should have a input regardless of props', () => {
+    const autocomplete = shallow(<Autocomplete />);
+    const input = autocomplete.find('input').last();
+    expect(input.length).toEqual(1);
 });
+
+it('should render a list based on the mocked data', () => {
+    const options = {createNodeMock};
+    let autocomplete; 
+    act(() => {
+        autocomplete = create(<Autocomplete data={ MockedData} />, options);
+    });
+    const testInstance = autocomplete.root;
+    const input = testInstance.findAll(
+        (el) => el.type === 'li'
+    );
+    expect(input.length).toEqual(5);
+});
+
+
