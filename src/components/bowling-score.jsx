ad194979c6053121954ef2c7e17874525b356582
DanN <dann@AMI-spare01s-MacBook-Pro.local>
2020-02-13 10:30:14 +0200

    bowling test

---
+++ src/components/bowling-score.jsx
@@ -0,0 +1,47 @@
+export const calculateBowlingScore = (numbers)  => {
+    let totalSum = 0;
+    const frames = calculateFrames(numbers);
+    frames.forEach((frame, index) => {
+        if (frame.length === 2) {
+            const isFrameStrike = isStrike(frame);
+            let sum = frame.reduce((a,b) => a + b, 0)
+            if (sum === 10 && frames[index + 1]) {
+                sum = sum + frames[index + 1][0]; 
+            } 
+            if(isFrameStrike && frames[index + 1] && frames[index + 2]) {
+                const tossToAdd = frames[index + 1][1] === 0 ? frames[index + 2][0] : frames[index + 1][1];
+                sum = sum + tossToAdd;
+            }
+            totalSum +=sum;
+        } else {
+            let sum = frame.reduce((a,b) => a + b, 0)
+            totalSum +=sum + frame[2];
+        }
+        console.log(totalSum);
+    })
+    return totalSum;
+}
+
+export const isStrike = (frame) => {
+    return frame[0] === 10
+}
+const isBigFrame = (frames) => {
+
+}
+export const calculateFrames = (tosses) => {
+    const results = [];
+    let frame = [];
+    tosses.forEach((toss, index) => {
+        if( index % 2 === 0) {
+            frame.push(toss);
+        } else {
+            frame.push(toss);
+            if(index === 19 && tosses[index +1]) {
+                frame.push(tosses[index +1])
+            }
+            results.push(frame);
+            frame = []
+        };
+    });
+    return results;
+}
---
+++ src/components/bowling-score.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { calculateBowlingScore } from './bowling-score';
+import { calculateFrames } from './bowling-score';
+import { create, act } from "react-test-renderer";
+
+describe('Bowling score', () => {
+    const firstData = [1, 1,1, 1, 1, 1,1, 1,1, 1,1, 1, 1, 1,1, 1,1, 1,1, 1];
+    const spareData = [9, 1,1, 1, 1, 1,1, 1,1, 1,1, 1, 1, 1,1, 1,1, 1,1, 1]
+    const strikeData = [10, 0,1, 1, 1, 1,1, 1,1, 1,1, 1, 1, 1,1, 1,1, 1,1, 1];
+    const perfectGame = [10, 0, 10, 0, 10, 0,10, 0,10, 0,10, 0, 10, 0,10, 0,10, 0,10, 10, 10];
+    const framesData = [9,1,3,4];
+    const realGame = [5, 4, 8, 2, 10, 0, 10, 0, 1, 0, 9, 1, 0, 10, 10, 0, 6, 4, 7, 3, 10];
+
+    it('should do normal sum without strikes or spares', () => {
+        expect(calculateBowlingScore(firstData)).toBe(20);
+    })
+    it('should calculate correct sum if we have a spare in a frame', () => {
+        expect(calculateBowlingScore(spareData)).toBe(29);
+    })
+    it('should calculate correct frames', () => {
+        expect(calculateFrames(framesData)).toEqual([[9,1] , [3,4]]);
+    })
+    it('should calculate correct strike', () => {
+        expect(calculateBowlingScore(strikeData)).toEqual(30);
+    });
+    it('should calculate perfect Score', () => {
+        expect(calculateBowlingScore(perfectGame)).toEqual(300);
+    })
+    fit('should calculate real Game Score', () => {
+        expect(calculateBowlingScore(realGame)).toEqual(149);
+    })
+
+    it('should calculate correct frames for odd tosses', () => {
+        const frames = calculateFrames(perfectGame);
+        expect(frames[frames.length - 1].length).toBe(3);
+    })
+});
