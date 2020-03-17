335327afd9854f5f0061bf6c76ef049333ac4662
Neciu Dan <dan@adoreme.com>
2020-03-17 21:15:46 +0200

    Solved the two sum algorithm

--- src/components/two-sum.jsx
+++ src/components/two-sum.jsx
@@ -1,3 +1,23 @@
 export const calculateTwoSumIndexes = (input, target) => {
-    
+    const originalInput = [...input];
+    const secondIndex = recursionFunction(input,target, originalInput);
+    return secondIndex;
+}
+
+const recursionFunction = (input, target, originalInput) => {
+    const value = input.shift();
+    const indexes = [];
+    indexes.push(originalInput.indexOf(value));
+    input.forEach((i) => {
+        if (i + value === target) {
+            indexes.push(originalInput.indexOf(i))
+        }
+    })
+
+    if(indexes.length === 2) {
+        return indexes;
+    } else {
+        const originalInputRecursion = [...originalInput];
+        return recursionFunction(input, target, originalInputRecursion)
+    }
 }
--- src/components/two-sum.test.js
+++ src/components/two-sum.test.js
@@ -5,7 +5,15 @@ describe('Two Sum Indexes', () => {
     const target = 9;
     const outputData = [0,1];
 
+    const inputData2 = [4,1,4,2,23];
+    const target2 = 3;
+    const outputData2 = [1,3];
+
     it('get the indexes that sum up to the target value', () => {
-        expect(calculateTwoSumIndexes(inputData, target)).toBe(outputData);
+        expect(calculateTwoSumIndexes(inputData, target)).toEqual(outputData);
+    })
+
+    it('get the indexes that sum up to the target value 2', () => {
+        expect(calculateTwoSumIndexes(inputData2, target2)).toEqual(outputData2);
     })
 });
