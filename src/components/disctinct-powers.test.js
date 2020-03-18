34e218eaf53e63a86ee89a4d4d0d2a8638ddc604
Neciu Dan <dan@adoreme.com>
2020-03-18 18:32:04 +0200

    solved another algorithm

--- src/components/disctinct-powers.test.js
+++ src/components/disctinct-powers.test.js
@@ -5,7 +5,15 @@ describe('Distinct Powers', () => {
     const b = [2,5];
     const outputData = [4,8,9,16,25,27,32,64,81,125,243,256,625,1024,3125];
 
+    const a2 = [1,2];
+    const b2 = [1,2];
+    const outputData2 = [1, 2, 4];
+
     it('get the distinct series in numerical order of all a at power b combinations', () => {
-        expect(calculateDistinctPowers(a, b)).toBe(outputData);
+        expect(calculateDistinctPowers(a, b)).toEqual(outputData);
+    })
+
+    it('get the distinct series in numerical order of all a at power b combinations second trye', () => {
+        expect(calculateDistinctPowers(a2, b2)).toEqual(outputData2);
     })
 });
--- src/components/distinct-powers.jsx
+++ src/components/distinct-powers.jsx
@@ -1,3 +1,12 @@
-export const calculateDistinctPowers = (input) => {
-    
+export const calculateDistinctPowers = (baseNumberRange, powerNumberRange) => {
+    const results = [];
+    for(let x=baseNumberRange[0]; x<= powerNumberRange[1]; x++) {
+        for(let y=powerNumberRange[0]; y<= powerNumberRange[1]; y++) {
+            let number = Math.pow(x, y);
+            if(results.indexOf(number) === -1) {
+                results.push(number);
+            }
+        }
+    }
+    return results.sort((a,b) => a < b ? -1 : 1)
 }
