5ab0a550571602ac9152b2b28472401251d8f149
Neciu Dan <dan@adoreme.com>
2020-03-22 21:15:41 +0200

    Solved algorithm nr 3

---
+++ src/algorithms/palindrom-product.jsx
@@ -0,0 +1,24 @@
+export const caulcateLargestPalindrom = (inputRange) => {
+    const largestNumber = (inputRange - 1) * (inputRange - 2);
+    for(let x = largestNumber; x > 0; x--) {
+        if(isPalindrom(x)) {
+            for(let y = inputRange -1; y > 0; y--) {
+                if (x % y === 0 && x / y < inputRange) {
+                    console.log(y);
+                    return x;
+                }
+            }
+        }
+    }
+
+}
+
+const isPalindrom = (number) => {
+    const string = number.toString();
+    for(let i = 0 ; i < string.length; i++) {
+        if(string[i] !== string[(string.length - 1) - i]) {
+            return false;
+        }
+    }
+    return true;
+}
--- src/algorithms/palindrom-product.test.js
+++ src/algorithms/palindrom-product.test.js
@@ -4,7 +4,14 @@ describe('Largest Palindrom Product', () => {
     const inputData = 100;
     const outputData = 9009;
 
+    const secondInputData = 1000;
+    const secondOutputData = 906609;
+
     it('get biggest palindrom number that is a product of two numbers smaller that the inputData', () => {
         expect(caulcateLargestPalindrom(inputData)).toBe(outputData);
+    })
+
+    it('get biggest palindrom number that is a product of two numbers smaller that the inputData', () => {
+        expect(caulcateLargestPalindrom(secondInputData)).toBe(secondOutputData);
     })
 });
