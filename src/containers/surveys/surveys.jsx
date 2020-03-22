55d96a7a0f3d45e7e5c8ff3718cef43b5fd94f1f
Neciu Dan <dan@adoreme.com>
2020-03-22 18:12:43 +0200

    Added future projects folders

---
+++ src/containers/surveys/surveys.jsx
@@ -0,0 +1,34 @@
+// User Stories — General
+// Survey Coordinators and Survey Respondents can define, conduct, and view surveys and survey results from a common website
+// Survey Coordinators can login to the app to access administrative functions, like defining a survey.
+
+// Defining a Survey
+
+// Survey Coordinator can define a survey containing 1–10 multiple choice questions.
+// Survey Coordinator can define 1–5 mutually exclusive selections to each question.
+// Survey Coordinator can enter a title for the survey.
+// Survey Coordinator can click a ‘Cancel’ button to return to the home page without saving the survey.
+// Survey Coordinator can click a ‘Save’ button save a survey.
+
+//Conducting a Survey
+
+// Survey Coordinator can open a survey by selecting a survey from a list of previously defined surveys
+// Survey Coordinators can close a survey by selecting it from a list of open surveys
+// Survey Respondent can complete a survey by selecting it from a list of open surveys
+// Survey Respondent can select responses to survey questions by clicking on a checkbox
+// Survey Respondents can see that a previously selected response will automatically be unchecked if a different response is clicked.
+// Survey Respondents can click a ‘Cancel’ button to return to the home page without submitting the survey.
+// Survey Respondents can click a ‘Submit’ button submit their responses to the survey.
+// Survey Respondents can see an error message if ‘Submit’ is clicked, but not all questions have been responded to.
+
+// Viewing Survey Results
+
+// Survey Coordinators and Survey Respondents can select the survey to display from a list of closed surveys
+// Survey Coordinators and Survey Respondents can view survey results as in tabular format showing the number of responses for each of the possible selections to the questions.
+
+// Bonus features
+
+// Survey Respondents can create a unique account in the app
+// Survey Respondents can login to the app
+// Survey Respondents cannot complete the same survey more than once
+// Survey Coordinators and Survey Respondents can view graphical representations of survey results (e.g. pie, bar, column, etc. charts)
